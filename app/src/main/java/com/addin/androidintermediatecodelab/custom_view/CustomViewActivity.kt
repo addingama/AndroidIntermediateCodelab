da4157c3154277aa4c6726137f465fd05b947e11
Addin Gama Bertaqwa <addingama@gmail.com>
2022-09-10 10:05:57 +0800

    Codelab custom button and edittext

--- app/src/main/java/com/addin/androidintermediatecodelab/custom_view/CustomViewActivity.kt
+++ app/src/main/java/com/addin/androidintermediatecodelab/custom_view/CustomViewActivity.kt
@@ -2,13 +2,42 @@ package com.addin.androidintermediatecodelab.custom_view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import com.addin.androidintermediatecodelab.R
 
 class CustomViewActivity : AppCompatActivity() {
+    private lateinit var myButton: MyButton
+    private lateinit var myEditText: MyEditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_custom_view)
 
         title = "Custom View"
+
+        myButton = findViewById(R.id.my_button)
+        myEditText = findViewById(R.id.my_edit_text)
+
+        setMyButtonEnable()
+
+        myEditText.addTextChangedListener(object: TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                setMyButtonEnable()
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+            }
+
+        })
+    }
+
+    private fun setMyButtonEnable() {
+        val result = myEditText.text
+        myButton.isEnabled = result != null && result.toString().isNotEmpty()
     }
 }
